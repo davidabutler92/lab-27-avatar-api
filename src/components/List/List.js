637611b56927e40606d8eee4ddf375d8438ae7e3
davidabutler92 <davidabutler92@yahoo.com>
2021-02-02 15:37:52 -0800

    add detail page with home link & listItem component for list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ListItem from '../ListItem/ListItem';
+import PropTypes from 'prop-types';
 
-export default function List() {
-  return (
-    <div>
-      
-    </div>
-  )
+function List({ characters }) {
+  const charactersList = characters.map((character) => {
+    return (
+      <li key={character._id}>
+        <Link to={`/detail/${character._id}`}>
+          <ListItem
+            key={character._id}
+            name={character.name}
+            photoUrl={character.photoUrl}
+          />
+        </Link>
+      </li>
+    );
+  });
+
+  return <ul>{charactersList}</ul>;
 }
+
+List.propTypes = {
+  characters: PropTypes.array,
+};
+
+export default List;
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ListItem() {
+function ListItem({ name, photoUrl }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <h1>{name}</h1>
+      <img src={photoUrl} alt='avatar character' />
+    </>
+  );
 }
+
+ListItem.propTypes = {
+  name: PropTypes.string,
+  photoUrl: PropTypes.string,
+}; 
+
+export default ListItem;
